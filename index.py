ce764bee4df709b8cc9c74fa49870b2a60712e5c
Michael J. Smith <michaeljoshuasmith1@gmail.com>
2021-06-05 16:58:42 -0400

    Added psuedocode for v1 web scraping

--- index.py
+++ index.py
@@ -4,12 +4,40 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait 
 from selenium.webdriver.support import expected_conditions as EC 
 
+from googlesearch import search
+
+
+# Read in .XSLX file in here
+
+# Process File, read in all names, return list/dict with all client information
+
+# traverse through all search results one by one
+
+    # Concatenate test to aide in search accuracy:
+        # Combine School District Name, Faculty Name, Phone # into single searchable text that we'll query via google.
+        
+        # grab first link
+
+        # call separate module for selenium to web parse this link
+
+        # this module will query all text strings with the @ symbol, this is most likely the email address of this person we're searching.
+
+        # append this email address to the data model that we've loaded into our script
+    # REPEAT TILL END OF LIST
+        fetch_link("")
+        res = search("PINE HILLS ELEMENTARY SCHOOL Tia Corniel")
+
+
+
+
+
+
 # initialize webdriver 
-PATH = "C:\\Users\\micha\\Downloads\\chromedriver_win32\\chromedriver.exe" 
-driver = webdriver.Chrome(PATH)
+# PATH = "C:\\Users\\micha\\Downloads\\chromedriver_win32\\chromedriver.exe" 
+# driver = webdriver.Chrome(PATH)
  
 # navigate to web page 
-driver.get("https://www.chufsd.org/Page/666") 
+# driver.get("https://www.chufsd.org/Page/666") 
 
 # locate search box 
 # search = driver.find_element(with_tag_name("input")) 
@@ -18,21 +46,21 @@ driver.get("https://www.chufsd.org/Page/666")
 # search.send_keys("staff") 
 # search.send_keys(Keys.RETURN) 
 
-try: 
-  # locate search results 
-  search_results = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "staffdirectorydiv"))) 
+# try: 
+#   # locate search results 
+#   search_results = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "staffdirectorydiv"))) 
 
-  # scrape posts headings 
-  facultyList = search_results.find_elements_by_css_selector("div.staff") 
+#   # scrape posts headings 
+#   facultyList = search_results.find_elements_by_css_selector("div.staff") 
 
-  for faculty in facultyList:
-    #   print("faculty = {}".format(faculty.get_attribute('innerHTML'))) 
-    name = faculty.find_element_by_css_selector("li.staffname")
-    print(name.text)
-    # name = faculty.find_elements_by_css_selector("staffname")
-    # header = post.find_element_by_tag_name("span")
-    # print(name)
+#   for faculty in facultyList:
+#     #   print("faculty = {}".format(faculty.get_attribute('innerHTML'))) 
+#     name = faculty.find_element_by_css_selector("li.staffname")
+#     print(name.text)
+#     # name = faculty.find_elements_by_css_selector("staffname")
+#     # header = post.find_element_by_tag_name("span")
+#     # print(name)
   
-finally: 
-  # quit browser 
-  driver.quit()
+# finally: 
+#   # quit browser 
+#   driver.quit()
