8642ee84e334a2c0c853340fbe4e60cf25c46633
Michael J. Smith <michaeljoshuasmith1@gmail.com>
2021-06-05 18:42:33 -0400

    Able to write email address to dataframe

--- index.py
+++ index.py
@@ -8,8 +8,23 @@ def fetch_input_file():
     df = pd.read_excel(path)
     return df
 
+# Write DataFrame to 
+def write_output_file(df):
+
+    # create excel writer object
+    writer = pd.ExcelWriter('nys-public-school-admins-with-email-contacts.xlsx')
+
+    # write dataframe to excel
+    df.to_excel(writer)
+
+    # save the excel
+    writer.save()
+
 # Process File, read in all names, return list/dict with all client information
 personsListDataFrame = fetch_input_file()
+
+# Must convert column for RelatedEmailAddresses from Float to String
+personsListDataFrame['RelatedEmailAddresses'] = personsListDataFrame['RelatedEmailAddresses'].astype('string')
 
 # traverse through all search results one by one
 for index, row in personsListDataFrame.iterrows():
@@ -20,31 +35,40 @@ for index, row in personsListDataFrame.iterrows():
     institutionName = row['InstitutionName']
     cso = row['CSO']
     searchStringText = "{}, {}".format(institutionName, cso) 
+    personsListDataFrame.at[index, 'RelatedEmailAddresses'] = 'michaeljoshuasmith1@gmail.com; michael.smith1@ge.com'
+    break
+
+write_output_file(personsListDataFrame)
 
     # print("Search String Text: {}".format(searchStringText))
 
-    # only search when "NAME" is present in CSO text
-    if "NOT AVAILABLE" not in searchStringText:
+    # handle case where "searchStringText" is not present in CSO text
+    # if "NOT AVAILABLE" not in searchStringText:
+    #     searchStringText = "{} Staff Directory".format(institutionName)
 
-        # grab first link
-        res = search(searchStringText)
+    # grab first link
+    # resultingWebPages = search(searchStringText)
 
-        # staff page: we're assuming google will give us the faculty page
-        facultyPage = res[0]
-
-        # call separate module for selenium to web parse this link
-        emailAddresses = fetch_email_address_by_webpage(facultyPage)
-
-        # convert to set such that we can remove dups
-        emailAddresses = list(set(emailAddresses))
+    # traverse through web pages
+    # for webpage in resultingWebPages:
 
 
-        print()
-        print("Email Addresses on webpage ({}) = {}".format(facultyPage, emailAddresses))
-        print()
-        # this module will query all text strings with the @ symbol, this is most likely the email address of this person we're searching.
+    # staff page: we're assuming google will give us the faculty page
+    # facultyPage = res[0]
 
-        # append this email address to the data model that we've loaded into our script
+    # call separate module for selenium to web parse this link
+    # emailAddresses = fetch_email_address_by_webpage(facultyPage)
+
+    # convert to set such that we can remove dups
+    # emailAddresses = list(set(emailAddresses))
+
+
+    # print()
+    # print("Email Addresses on webpage ({}) = {}".format(facultyPage, emailAddresses))
+    # print()
+    # this module will query all text strings with the @ symbol, this is most likely the email address of this person we're searching.
+
+    # append this email address to the data model that we've loaded into our script
     
     # REPEAT TILL END OF LIST
         # fetch_link("")
