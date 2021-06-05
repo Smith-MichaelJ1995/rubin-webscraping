36bd46876c8d399e0aa8ca58aed1f939c4fd614d
Michael J. Smith <michaeljoshuasmith1@gmail.com>
2021-06-05 17:32:05 -0400

    Able to generate valid search string for google python library

--- index.py
+++ index.py
@@ -9,15 +9,20 @@ def fetch_input_file():
     return df
 
 # Process File, read in all names, return list/dict with all client information
-facultyList = fetch_input_file()
-
-print("facultyList: {}".format(facultyList))
+personsListDataFrame = fetch_input_file()
 
 # traverse through all search results one by one
+for index, row in personsListDataFrame.iterrows():
 
     # Concatenate test to aide in search accuracy:
-        # Combine School District Name, Faculty Name, Phone # into single searchable text that we'll query via google.
-        
+    # Combine School District Name, Faculty Name, Phone # into single searchable text that we'll query via google.
+    # fetching critical values
+    institutionName = row['InstitutionName']
+    cso = row['CSO']
+    searchStringText = "{}, {}".format(institutionName, cso) 
+
+    # print("Search String Text: {}".format(searchStringText))
+
         # grab first link
 
         # call separate module for selenium to web parse this link
