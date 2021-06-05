b0ef414d8d951101a19220b4eb5a385156dd4c16
Michael J. Smith <michaeljoshuasmith1@gmail.com>
2021-06-05 16:33:26 -0400

    Can fetch faculty information from pre-defined webpage using tags that are known beforehand.

--- index.py
+++ index.py
@@ -9,25 +9,29 @@ PATH = "C:\\Users\\micha\\Downloads\\chromedriver_win32\\chromedriver.exe"
 driver = webdriver.Chrome(PATH)
  
 # navigate to web page 
-driver.get("https://www.reddit.com/") 
+driver.get("https://www.chufsd.org/Page/666") 
 
 # locate search box 
-search = driver.find_element_by_name("q") 
+# search = driver.find_element(with_tag_name("input")) 
 
 # enter search term 
-search.send_keys("scraping") 
-search.send_keys(Keys.RETURN) 
+# search.send_keys("staff") 
+# search.send_keys(Keys.RETURN) 
 
 try: 
   # locate search results 
-  search_results = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "rpBJOHq2PR60pnwJlUyP0"))) 
+  search_results = WebDriverWait(driver, 20).until(EC.presence_of_element_located((By.CLASS_NAME, "staffdirectorydiv"))) 
 
   # scrape posts headings 
-  posts = search_results.find_elements_by_css_selector("h3._eYtD2XCVieq6emjKBH3m") 
+  facultyList = search_results.find_elements_by_css_selector("div.staff") 
 
-  for post in posts: 
-    header = post.find_element_by_tag_name("span")
-    print(header.text)
+  for faculty in facultyList:
+    #   print("faculty = {}".format(faculty.get_attribute('innerHTML'))) 
+    name = faculty.find_element_by_css_selector("li.staffname")
+    print(name.text)
+    # name = faculty.find_elements_by_css_selector("staffname")
+    # header = post.find_element_by_tag_name("span")
+    # print(name)
   
 finally: 
   # quit browser 
