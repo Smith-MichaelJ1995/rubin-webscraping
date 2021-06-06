475cef8ceaf8b2ffcf8f53330f591ca84c6364a9
Michael J. Smith <michaeljoshuasmith1@gmail.com>
2021-06-05 20:23:54 -0400

    Essentially finished with first prototype: Can traverse through all emails and record results.. Having strange error with Master directory.. Iterating through keys: Worked 8 Hours.

--- index.py
+++ index.py
@@ -4,15 +4,28 @@ import pandas as pd
 
 # Read in .XSLX file in here
 def fetch_input_file():
+
+    # specify path of provided NYS School Admins Directory
     path = "SEDdir.xls"
+
+    # process input file into dataframe
     df = pd.read_excel(path)
+
+    # Must convert column for RelatedEmailAddresses from Float to String
+    df['RelatedEmailAddresses'] = df['RelatedEmailAddresses'].astype('string')
+
+    # return dataframe to caller
     return df
 
 # Write DataFrame to 
-def write_output_file(df):
+def write_output_file(df, fileName):
+
+    # convert dict to DataFrame if neccessary
+    if type(df) is dict:
+        df = pd.DataFrame(data=df, index=[0])
 
     # create excel writer object
-    writer = pd.ExcelWriter('nys-public-school-admins-with-email-contacts.xlsx')
+    writer = pd.ExcelWriter(fileName)
 
     # write dataframe to excel
     df.to_excel(writer)
@@ -20,47 +33,113 @@ def write_output_file(df):
     # save the excel
     writer.save()
 
+# Given District Directory Data Structure & Provided Email Address, Determine if this new email address should be added to list
+def hasThisEmailAddressBeenReturnedForPriorSearchStringText(masterDistrictDirectory, person, domain):
+
+    # have we identified this domain before?
+    if domain in masterDistrictDirectory:
+
+        # traverse through all email addresses identified for this domain
+        for previouslyDefinedEmailAddress in masterDistrictDirectory[domain]:
+
+            # does this person's email address exist already in the list of emails we've captured for this school district?
+            if person in previouslyDefinedEmailAddress:
+
+                # inform caller that we should not add this emailAddress to the list
+                return True, masterDistrictDirectory
+        
+        # if we've reached this point, then the proposed person has not been identified within the "previouslyDefinedEmailAddress"
+        return False, masterDistrictDirectory
+
+    else:
+
+        # domain has not been identified before, create new entry for it in our masterDistrictDictionary
+        masterDistrictDirectory[domain] = []
+        
+        # inform caller that we should add this emailAddress to the list
+        return False, masterDistrictDirectory
+        
+
+
+# District Directory Data Structure: Each Key is a unique district, each Value is a list of unique email addresses belonging to that district.
+masterDistrictDirectory = {}
+
 # Process File, read in all names, return list/dict with all client information
 personsListDataFrame = fetch_input_file()
-
-# Must convert column for RelatedEmailAddresses from Float to String
-personsListDataFrame['RelatedEmailAddresses'] = personsListDataFrame['RelatedEmailAddresses'].astype('string')
 
 # traverse through all search results one by one
 for index, row in personsListDataFrame.iterrows():
 
-    # Concatenate test to aide in search accuracy:
+    # Capture All Email Address Results For This Person
+    emailAddressResultsForThisPerson = []
+
     # Combine School District Name, Faculty Name, Phone # into single searchable text that we'll query via google.
-    # fetching critical values
+    # Extract InstitutionName & CSO From Current Data Element
     institutionName = row['InstitutionName']
     cso = row['CSO']
+
+    # CLEARLY INDICATE TO CALLER THAT WE'RE PROCESSING A NEW PERSON
+    print()
+    print("###########################################################")
+    print("PROCESSING NEW INDIVIDUAL: INDEX = {}, PERSON = {}".format(index, cso))
+
+    # Generate Search String Text
     searchStringText = "{}, {}".format(institutionName, cso) 
-    personsListDataFrame.at[index, 'RelatedEmailAddresses'] = 'michaeljoshuasmith1@gmail.com; michael.smith1@ge.com'
-    break
-
-write_output_file(personsListDataFrame)
-
-    # print("Search String Text: {}".format(searchStringText))
 
     # handle case where "searchStringText" is not present in CSO text
-    # if "NOT AVAILABLE" not in searchStringText:
-    #     searchStringText = "{} Staff Directory".format(institutionName)
+    if "NOT AVAILABLE" not in searchStringText:
+        searchStringText = "{} Staff Directory".format(institutionName)
 
-    # grab first link
-    # resultingWebPages = search(searchStringText)
+    # search web for all pages related to search text
+    resultingWebPages = search(searchStringText)
+
+    print("Total # Web Pages Returned For '{}', Size = {}".format(searchStringText, len(resultingWebPages)))
 
     # traverse through web pages
-    # for webpage in resultingWebPages:
+    for index, webpage in enumerate(resultingWebPages):
 
+        # CLEARLY INDICATE TO CALLER THAT WE'RE PROCESSING A NEW PERSON
+        print("PROCESSING NEW WEBPAGE: INDEX = {}, PERSON = {}".format(index, webpage))
+        
+        # call separate selenium module to gather all email addresses located on this webpage
+        emailAddressesForThisWebPage = fetch_email_address_by_webpage(webpage)
 
-    # staff page: we're assuming google will give us the faculty page
-    # facultyPage = res[0]
+        # remove duplicates within email address list
+        emailAddressesForThisWebPage = list(set(emailAddressesForThisWebPage))
+        
+        # traverse all email addresses returned by this webpage
+        for emailAddress in emailAddressesForThisWebPage:
 
-    # call separate module for selenium to web parse this link
-    # emailAddresses = fetch_email_address_by_webpage(facultyPage)
+            # has this perticular email address been identified through a previous page we've searched (on this searchStringText)? 
+            if emailAddress in emailAddressResultsForThisPerson:
+                print("Ignoring Email Address: {}, it has been identified on a prior webpage we found when searching for '{}'".format(emailAddress, searchStringText))
+            else:
 
-    # convert to set such that we can remove dups
-    # emailAddresses = list(set(emailAddresses))
+                # extrapolate district domain name
+                person, domain = emailAddress.split("@")
+                masterDistrictDirectory, duplicate = hasThisEmailAddressBeenReturnedForPriorSearchStringText(masterDistrictDirectory, person, domain)
+
+                # has this perticular email address been identified through results of a previous searchStringText?
+                if duplicate:
+                    print("Igoring Email Address: {}, it has been identified on a prior search for a previous 'searchStringText'".format(emailAddress)) 
+                else:
+
+                    # WE'VE CONFIRMED THAT THIS EMAIL ADDRESS WE'VE FOUND IS UNIQUE: 
+                    # Has not been found during searches for prior people or prior webpages on the search for this person 
+                    masterDistrictDirectory[domain].append(emailAddress)
+                    emailAddressResultsForThisPerson.append(emailAddress)
+                    print("Unique Email Address Found Related to Search String ({}): {}".format(searchStringText, emailAddress))
+
+    # print("Resulting Email Addresses For This Person Are: {}".format(emailAddressResultsForThisPerson))
+    personsListDataFrame.at[index, 'RelatedEmailAddresses'] = str(emailAddressResultsForThisPerson)
+
+    break
+
+# Script Has Finished Executing, Write Resulting Objects To File
+write_output_file(personsListDataFrame, 'nys-public-school-admins-with-related-email-contacts.xls')
+
+# Write Resulting Master Directory Structure to XLS
+write_output_file(masterDistrictDirectory, 'nys-public-school-admin-with-related-email-contacts-by-district.xls')
 
 
     # print()
