b6aeac691e3fbc31240b1acca9062f736c591295
Michael J. Smith <michaeljoshuasmith1@gmail.com>
2021-09-19 11:08:23 -0400

    Able to avoid 429 error by switching to personal hotspot (works even on same home wifi network) and setting time from 1-90.. Now attempting 1-75.

--- src/Phase2.py
+++ src/Phase2.py
@@ -30,8 +30,16 @@ class Phase2:
     # extract profile link url
     def fetch_profilelink_for_poi(self):
 
+        print("Total Persons Of Interest: {}".format(len(self.records["oon"] + self.records["inn"])))
+        print("Total Persons Of Interest Out-Of-Network: {}".format(len(self.records["oon"])))
+
         # traverse through all poi's out-of-network
         for idx, person in enumerate(self.records["oon"]):
+
+            if idx == 50:
+                print("quitting.. writing to filesystem")
+                self.write_json_output_file("../current-data/vocational-poi-names-generated.json", self.records["oon"] + self.records["inn"])
+                exit()
 
             print("processing index: {}".format(idx))
 
@@ -51,6 +59,8 @@ class Phase2:
             except Exception as e:
                 print("The Following Error Has Occured: {}".format(e))
                 print("Writing To Filesystem...")
+                # write persons_of_interest data structure to filesystem
+                self.write_json_output_file("../current-data/vocational-poi-names-generated.json", self.records["oon"] + self.records["inn"])
                 exit()
                     
             # handle resulting webpages
@@ -62,7 +72,8 @@ class Phase2:
                     print(targetProfileURL)
                     break
                 else:
-                    print("passing over non-profile link: {}".format(link))
+                    pass
+                    #print("passing over non-profile link: {}".format(link))
                     
             # have we found a best-fit profile?
             if targetProfileURL != "":
@@ -91,12 +102,6 @@ class Phase2:
                 # print updated record
                 print(person)
 
-                # slow down search too avoid 429 too many requests
-                pauseTime = random.randint(1, 60)
-                print("Sleeping for {} seconds".format(pauseTime))
-                print("")
-                time.sleep(pauseTime)
-
             else:
                 print("No Results Found For Search: '{}'".format(searchStringText))
                 # print("")
@@ -114,6 +119,12 @@ class Phase2:
                 #     # update profileURL & name
                 #     person['profileUrl'] = profileUrl
                 #     person['fullName'] = fullName
+
+            # slow down search too avoid 429 too many requests
+            pauseTime = random.randint(1, 75)
+            print("Sleeping for {} seconds".format(pauseTime))
+            print("")
+            time.sleep(pauseTime)
 
             
     
