8c64060d3dea384b522bc74031a7cc070aa7c0a1
Michael J. Smith <michaeljoshuasmith1@gmail.com>
2021-09-19 15:36:48 -0400

    Phase2 finished.

--- src/Phase2.py
+++ src/Phase2.py
@@ -22,11 +22,43 @@ class Phase2:
         self.is_name_missing_from_poi(vocational_poi)
 
         # traverse through all poi that have the error key
-        self.fetch_profilelink_for_poi()
+        #self.fetch_profilelink_for_poi()
+
+        # handle missing persons who could not be resolved
+        self.resolve_profilelink_for_unresolved_poi()
 
         # write persons_of_interest data structure to filesystem
         self.write_json_output_file("../current-data/vocational-poi-names-generated.json", self.records["oon"] + self.records["inn"])
 
+    # given unresolvable names from list, resort to setting these fields for each person manually
+    def resolve_profilelink_for_unresolved_poi(self):
+
+        # notify user of need to manually set records
+        print("Will Need To Resolve The Following Number of OON ppl manually: {}".format(len(self.records["oon"])))
+
+        # traverse through all poi's out-of-network
+        for idx, person in enumerate(self.records["oon"]):
+        
+            # notify user which person we're tracking
+            print("processing index: {}".format(idx))
+
+            # set position=currentJob by default, if this key is empty, then use job
+            position = person['currentJob'] if len(person['currentJob']) > 0 else person['job']
+
+            # create placeholder for search string text
+            searchStringText = "{}, Linkedin Profile, {}".format(position, person['location'])
+
+            # set fields based on user input
+            print("No Results Found For Search: '{}'".format(searchStringText))
+            person['profileUrl'] = input("Profile URL:")
+            person['fullName'] = input("Full Name:")
+
+            # remove error key from our dict
+            person.pop("error")
+
+            # create additional cursor space
+            print("")
+    
     # extract profile link url
     def fetch_profilelink_for_poi(self):
 
@@ -41,7 +73,7 @@ class Phase2:
             #     self.write_json_output_file("../current-data/vocational-poi-names-generated.json", self.records["oon"] + self.records["inn"])
             #     exit()
 
-            print("processing index: {}".format(idx))
+            #print("processing index: {}".format(idx))
 
             # blank targetProfileURL variable
             targetProfileURL = ""
