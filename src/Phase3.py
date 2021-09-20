fcdf02e58cbdbeb7883fa4742a617aafcb041410
Michael J. Smith <michaeljoshuasmith1@gmail.com>
2021-09-19 21:22:38 -0400

    Phase3.py complete, can compute the employers of all poi

--- src/Phase3.py
+++ src/Phase3.py
@@ -9,14 +9,17 @@ import pandas as pd
 class Phase3:
     def __init__(self, path):
 
+        # inform user that we're starting pipeline..
+        print("STARTING PHASE 3... Resolve Employer For Educating Director")
+
         # Process File, read in all names, return list/dict with all client information
-        self.personsListDataFrame = self.fetch_json_input_file(path)
+        self.records = self.fetch_json_input_file(path)
 
         # iterating through all admins, search for contacts and log them
         self.run()
 
         # write outputs to .xslx
-        write_output_file('cte-educators-admins.xls', self.personsListDataFrame)
+        self.write_json_output_file("../current-data/vocational-poi-with-employers.json", self.records)
 
     # Read in vocational-poi.json from path
     def fetch_json_input_file(self, path):
@@ -31,6 +34,26 @@ class Phase3:
     # given the dataframe of admins - generated from .xls file attached, iterate through each person
     # then begin searching for emails
     def run(self):
+
+        # traverse through all poi's out-of-network
+        for idx, person in enumerate(self.records):
+
+            # create placeholder for position of person
+            position = ""
+            separator = "at "
+
+            # calculate employer based on "at" string in currentJob/job field
+            if len(person['currentJob']) > 0 and separator in person['currentJob']:
+                position = person['currentJob'].split(separator)[-1]
+            elif len(person['job']) > 0 and separator in person['job']:
+                position = person['job'].split(separator)[-1]
+            else:
+                position = "None"
+
+            # set person's employer field equal to the position we've derived in step above
+            person['employer'] = position
+
+
 
         
      
