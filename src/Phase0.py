1324ff4067c82e55d798a730bbb175c3c820eac6
Michael J. Smith <michaeljoshuasmith1@gmail.com>
2021-11-14 20:58:06 -0500

    1) Able to read in location codes spreadsheet and parse out codes which have State,USA.  2) Now will traverse through all search terms to dynamically build out all links.

--- src/Phase0.py
+++ src/Phase0.py
@@ -5,28 +5,20 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC 
 import pandas as pd
 
+import openpyxl
+from pathlib import Path
+
 
 class Phase0:
 
-    # Write DataFrame to xlsx
-    def write_output_file(self, fileName, df):
-
-        # create excel writer object
-        writer = pd.ExcelWriter(fileName)
-
-        # write dataframe to excel
-        df.to_excel(writer)
-
-        # save the excel
-        writer.save()
-
-    def __init__(self):
+    # build locale spreadsheet from provided file
+    def generate_locale_file_from_system(self, path):
 
         # placeholder for data to be added to the speadsheet
         finalResults = []
 
         # fetch locale codes from filesystem
-        localCodesRawFile = open("../linkedin-location-codes.txt", "r")
+        localCodesRawFile = open(path, "r")
 
         # iterate through all lines in the file
         for line in localCodesRawFile:
@@ -50,9 +42,53 @@ class Phase0:
         df.to_excel(writer)
 
         # save the excel
-        writer.save()      
+        writer.save() 
+
+    def __init__(self):  
+
+        # stage danny's keywords for search
+        keywords = [
+            "business teacher",
+            "marketing teacher",
+            "CTAE director",
+            "CTE teacher",
+            "business professor",
+            "marketing professor",
+            "entrepreneurship teacher",
+            "CTE coordinator"
+        ]   
+
+        # fetch location codes file from system, convert into iterable object
+        xlsx_file = Path('location-codes-datatable.xlsx')
+        wb_obj = openpyxl.load_workbook(xlsx_file)
+        sheet = wb_obj.active
+
+        # traverse through all rows in spreadsheet
+        for row in sheet.iter_rows(2, sheet.max_row):
+
+            # extract key values from row
+            cellLocation = row[1]
+            cellCountry = row[2]
+            cellCode = row[3]
+
+            # only record of country code is US
+            if "United States" in cellLocation.value and cellCountry.value == "US":
+
+                # determine if location code is a state
+                if cellLocation.value.count(",") == 1:
+                    
+                    
+                    print(cellLocation.value)
 
 
+            #print("cellA: {}".format(cellA.value))
+            #print("cellLocation: {}".format(cellLocation.value))
+            #print("cellCountry: {}".format(cellCountry.value))
+            #print("cellCode: {}".format(cellCode.value))
+
+            #exit()
+            #for cell in row:
+                #print(cell.value)
         
 
         # signify different states in the US
